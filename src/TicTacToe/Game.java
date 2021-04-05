6620f198c0ba3aac1ee503071b135817cf206a7e
Damilare Jolayemi <djolayemi@gmail.com>
2021-04-05 19:14:56 +0100

    Exercise

--- src/TicTacToe/Game.java
+++ src/TicTacToe/Game.java
@@ -2,54 +2,95 @@ package TicTacToe;
 
 import java.util.Arrays;
 import java.util.Random;
+import static TicTacToe.Options.*;
 
 public class Game {
-    private final String[] gameBoard = new String[9];
-    private boolean playerHasPlayed;
+    private final Options[][] gameBoard = new Options[3][3];
+    private boolean playerOneHasPlayed;
     private boolean computerHasPlayed;
     private boolean playerWin;
     private boolean computerWin;
+    private boolean playerTwoHasPlayed;
 
-    public String visualizeBoard() {
-        StringBuilder board = new StringBuilder();
-        for (int i = 1; i <= gameBoard.length; i++) {
-            board.append(gameBoard[i - 1]);
-            if(i % 3 == 0)
-                board.append("|\n");
+    public Game() {
+        for (Options[] options : gameBoard) {
+            Arrays.fill(options, EMPTY);
         }
-        return board.toString();
+    }
+    
+    public String visualizeBoard() {
+        String display = "";
+        for (int i = 0; i < gameBoard.length; i++) {
+            for (int j = 0; j < gameBoard[i].length; j++) {
+                if(j != gameBoard[i].length - 1)
+                display += gameBoard[i][j] + " ";
+
+                else
+                    display += gameBoard[i][j] + "\n";
+            }
+
+        }
+        return display;
     }
 
-    public void designBoardPattern() {
-        Arrays.fill(gameBoard, "|___");
+    
+
+    public void playerOneGamePlay(int position) {
+        validatePosition(position);
+
+        int column = (position - 1) % 3;
+        int row = (position - 1) / 3;
+
+        checkPreviouslyPlayedPosition(position, gameBoard[row][column]);
+
+        gameBoard[row][column] = O;
+        playerOneHasPlayed = true;
+
     }
 
-    public void play(int position) {
-        boolean positionOccupied = gameBoard[position - 1].equals("| O ") || gameBoard[position - 1].equals("| X ");
-        if(positionOccupied)
+    private void validatePosition(int position) {
+        if(position < 1 || position > 9)
+            throw new IndexOutOfBoundsException("Invalid position");
+    }
+
+    public void playerTwoGamePlay(int position) {
+        validatePosition(position);
+
+        int column = (position - 1) % 3;
+        int row = (position - 1) / 3;
+        checkPreviouslyPlayedPosition(position, gameBoard[row][column]);
+
+        gameBoard[row][column] = X;
+        playerTwoHasPlayed = true;
+
+    }
+
+    private void checkPreviouslyPlayedPosition(int position, Options options) {
+        boolean positionOccupied = options.equals(O) || options.equals(X);
+        if (positionOccupied)
             throw new IllegalArgumentException("Position " + position + " has already been played");
-
-        gameBoard[position - 1] = "| O ";
-        playerHasPlayed = true;
-
     }
 
     public void computerPlay() {
         Random random = new Random();
         int generatedPosition;
+        int column;
+        int row;
 
-//        do{
+        do{
             generatedPosition = 1 + random.nextInt(9);
-//        }
-//        while(!(gameBoard[generatedPosition - 1].equals("|___ "))) ;
+            column = (generatedPosition - 1) % 3;
+            row = (generatedPosition - 1) / 3;
+        }
+        while(!(gameBoard[row][column].equals(EMPTY))) ;
 
-        gameBoard[generatedPosition - 1] = "| X ";
-        playerHasPlayed = true;
+
+        gameBoard[row][column] = X;
        computerHasPlayed = true;
     }
 
-    public boolean playerHasPlayed() {
-        return playerHasPlayed;
+    public boolean playerOneHasPlayed() {
+        return playerOneHasPlayed;
     }
 
     public boolean computerHasPlayed() {
@@ -67,10 +108,10 @@ public class Game {
     }
 
     private void checkDiagonal(){
-        boolean computerFirstDiagonalCheck = gameBoard[0].equals("| X ") && gameBoard[4].equals("| X ") && gameBoard[8].equals("| X ");
-        boolean computerSecondDiagonalCheck = gameBoard[2].equals("| X ") && gameBoard[4].equals("| X ") && gameBoard[6].equals("| X ");
-        boolean playerFirstDiagonalCheck = gameBoard[0].equals("| O ") && gameBoard[4].equals("| O ") && gameBoard[8].equals("| O ");
-        boolean playerSecondDiagonalCheck = gameBoard[2].equals("| O ") && gameBoard[4].equals("| O ") && gameBoard[6].equals("| O ");
+        boolean computerFirstDiagonalCheck = gameBoard[0][0].equals(X) && gameBoard[1][1].equals(X) && gameBoard[2][2].equals(X);
+        boolean computerSecondDiagonalCheck = gameBoard[0][2].equals(X) && gameBoard[1][1].equals(X) && gameBoard[2][0].equals(X);
+        boolean playerFirstDiagonalCheck = gameBoard[0][0].equals(O) && gameBoard[1][1].equals(O) && gameBoard[2][2].equals(O);
+        boolean playerSecondDiagonalCheck = gameBoard[0][2].equals(O) && gameBoard[1][1].equals(O) && gameBoard[2][0].equals(O);
 
         if(playerFirstDiagonalCheck || playerSecondDiagonalCheck)
             playerWin = true;
@@ -81,8 +122,8 @@ public class Game {
     }
 
     private void checkFirstColumn(){
-        boolean computerFirstColumnCheck = gameBoard[0].equals("| X ") && gameBoard[3].equals("| X ") && gameBoard[6].equals("| X ");
-        boolean playerFirstColumnCheck = gameBoard[0].equals("| O ") && gameBoard[3].equals("| O ") && gameBoard[6].equals("| O ");
+        boolean computerFirstColumnCheck = gameBoard[0][0].equals(X) && gameBoard[1][0].equals(X) && gameBoard[2][0].equals(X);
+        boolean playerFirstColumnCheck = gameBoard[0][0].equals(O) && gameBoard[1][0].equals(O) && gameBoard[2][0].equals(O);
         if(playerFirstColumnCheck)
             playerWin = true;
         else if(computerFirstColumnCheck)
@@ -90,8 +131,8 @@ public class Game {
 
     }
     private void checkSecondColumn(){
-        boolean computerSecondColumnCheck = gameBoard[1].equals("| X ") && gameBoard[4].equals("| X ") && gameBoard[7].equals("| X ");
-        boolean playerSecondColumnCheck = gameBoard[1].equals("| O ") && gameBoard[4].equals("| O ") && gameBoard[7].equals("| O ");
+        boolean computerSecondColumnCheck = gameBoard[0][1].equals(X) && gameBoard[1][1].equals(X) && gameBoard[2][1].equals(X);
+        boolean playerSecondColumnCheck = gameBoard[0][1].equals(O) && gameBoard[1][1].equals(O) && gameBoard[2][1].equals(O);
         if(playerSecondColumnCheck)
             playerWin = true;
 
@@ -100,8 +141,8 @@ public class Game {
 
     }
     private void checkThirdColumn(){
-        boolean computerThirdColumnCheck = gameBoard[2].equals("| X ") && gameBoard[5].equals("| X ") && gameBoard[8].equals("| X ");
-        boolean playerThirdColumnCheck = gameBoard[2].equals("| O ") && gameBoard[5].equals("| O ") && gameBoard[8].equals("| O ");
+        boolean computerThirdColumnCheck = gameBoard[0][2].equals(X) && gameBoard[1][2].equals(X) && gameBoard[2][2].equals(X);
+        boolean playerThirdColumnCheck = gameBoard[0][2].equals(O) && gameBoard[1][2].equals(O) && gameBoard[2][2].equals(O);
         if(playerThirdColumnCheck)
             playerWin = true;
 
@@ -110,9 +151,9 @@ public class Game {
 
     }
 
-    private void checkFirstRow(){
-        boolean computerFirstRowCheck = gameBoard[0].equals("| X ") && gameBoard[1].equals("| X ") && gameBoard[2].equals("| X ");
-        boolean playerFirstRowCheck = gameBoard[0].equals("| O ") && gameBoard[1].equals("| O ") && gameBoard[2].equals("| O ");
+    private void checkSecondRow(){
+        boolean computerFirstRowCheck = gameBoard[1][0].equals(X) && gameBoard[1][1].equals(X) && gameBoard[1][2].equals(X);
+        boolean playerFirstRowCheck = gameBoard[1][0].equals(O) && gameBoard[1][1].equals(O) && gameBoard[1][2].equals(O);
         if(playerFirstRowCheck)
             playerWin = true;
 
@@ -121,9 +162,9 @@ public class Game {
 
     }
 
-    private void checkSecondRow(){
-        boolean computerSecondRowCheck = gameBoard[3].equals("| X ") && gameBoard[4].equals("| X ") && gameBoard[5].equals("| X ");
-        boolean playerSecondCheck = gameBoard[3].equals("| O ") && gameBoard[4].equals("| O ") && gameBoard[5].equals("| O ");
+    private void checkFirstRow(){
+        boolean computerSecondRowCheck = gameBoard[0][0].equals(X) && gameBoard[0][1].equals(X) && gameBoard[0][2].equals(X);
+        boolean playerSecondCheck = gameBoard[0][0].equals(O) && gameBoard[0][1].equals(O) && gameBoard[0][2].equals(O);
         if(playerSecondCheck)
             playerWin = true;
 
@@ -132,8 +173,8 @@ public class Game {
     }
 
     private void checkThirdRow(){
-        boolean computerThirdRowCheck = gameBoard[6].equals("| X ") && gameBoard[7].equals("| X ") && gameBoard[8].equals("| X ");
-        boolean playerThirdRowCheck = gameBoard[6].equals("| O ") && gameBoard[7].equals("| O ") && gameBoard[8].equals("| O ");
+        boolean computerThirdRowCheck = gameBoard[2][0].equals(X) && gameBoard[2][1].equals(X) && gameBoard[2][2].equals(X);
+        boolean playerThirdRowCheck = gameBoard[2][0].equals(O) && gameBoard[2][1].equals(O) && gameBoard[2][2].equals(O);
         if(playerThirdRowCheck)
             playerWin = true;
 
@@ -152,5 +193,7 @@ public class Game {
     }
 
 
-
+    public boolean playerTwoHasPlayed() {
+        return playerTwoHasPlayed;
+    }
 }
